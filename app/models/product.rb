8e3b3d72f44d22fed7e78a1fb2496a5b9777e399
aqibqayyum1 <aqib.qayyum@gems.techverx.com>
2018-03-28 11:31:50 +0500

    validates product name

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,7 @@ resourcify
 belongs_to :category
 has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
-
+validates :name, presence: true
 has_many :order_items
 has_many :line_items, dependent: :destroy
 
